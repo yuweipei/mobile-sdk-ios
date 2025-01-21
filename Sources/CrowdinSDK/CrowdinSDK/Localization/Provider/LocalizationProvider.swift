bbb36c124b63401a9515d03b606f447967767d30
David Pei <david.pei.philly@gmail.com>
2025-01-21 13:24:59 -0500

    fix typo

--- Sources/CrowdinSDK/CrowdinSDK/Localization/Provider/LocalizationProvider.swift
+++ Sources/CrowdinSDK/CrowdinSDK/Localization/Provider/LocalizationProvider.swift
@@ -84,9 +84,9 @@ class LocalizationProvider: NSObject, LocalizationProviderProtocol {
         }
     }
     
-    func prepare(with completion:  @escaping () -> Void) {
+    func prepare(with completion: @escaping () -> Void) {
         let shouldFetchLocalization = self.localizations.count == 0 // Remote storage doesn't contain any languages. Probably first run, no information about supported localizations.
-        remoteStorage.prepare { weak self] in
+        remoteStorage.prepare { [weak self] in
             guard let self = self else { return }
             if shouldFetchLocalization {
                 self.refreshLocalization { _ in
